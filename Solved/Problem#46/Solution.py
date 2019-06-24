007ff5e81ab5a43f3e2d2c6310c27f771a96af0e
SubhoBasak <subhobasak50@gmail.com>
2019-06-24 13:49:08 +0530

    added the testing part

--- Solved/Problem#46/Solution.py
+++ Solved/Problem#46/Solution.py
@@ -4,8 +4,71 @@ def evaluate_bin_tree_exp(root):
     if root.value in ['+', '-', '*', '/']:
         left_val = evaluate_bin_tree_exp(root.left_child)
         right_val = evaluate_bin_tree_exp(root.right_child)
-        return eval(str(left_val)+root.value+str(right_value))
+        return eval(str(left_val)+root.value+str(right_val))
     else:
         return root.value
 
-# Testing part will be soon...
+# Testing part
+
+class node:
+    def __init__(self, value = None):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+def create(st):
+    try:
+        operand = float(st)
+        return node(operand)
+
+    except ValueError:
+        parentheses = 0
+        for i, j in enumerate(st):
+            if j == '(':
+                parentheses += 1
+            elif j == ')':
+                if parentheses > 0:
+                    parentheses -= 1
+                else:
+                    print('\'(\' is missing before \')\'')
+                    return None
+            elif (j == '+' or j == '-') and parentheses == 0:
+                root = node(j)
+                root.left_child = create(st[:i])
+                root.right_child = create(st[i+1:])
+                return root
+
+        parentheses = 0
+        for i, j in enumerate(st):
+            if j == '(':
+                parentheses += 1
+            elif j == ')':
+                if parentheses > 0:
+                    parentheses -= 1
+                else:
+                    print('\'(\' is missing before \')\'')
+                    return None
+            elif (j == '*' or j == '/') and parentheses == 0:
+                root = node(j)
+                root.left_child = create(st[:i])
+                root.right_child = create(st[i+1:])
+                return root
+
+        parentheses = 0
+        for i, j in enumerate(st):
+            if j == '(':
+                parentheses += 1
+            elif j == ')':
+                if parentheses > 0:
+                    parentheses -= 1
+                else:
+                    print('\'(\' is missing before \')\'')
+                    return None
+            elif (j == '^' or j == '**') and parentheses == 0:
+                root = node(j)
+                root.left_child = create(st[:i])
+                root.right_child = create(st[i+1:])
+                return root
+
+        st = st.lstrip('(').rstrip(')')
+        return create(st)
